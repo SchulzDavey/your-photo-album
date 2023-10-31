1745a50fcb46ddab2e8430355dfcdbffd47952c3
Davey Schulz <davey.schulz@hotmail.com>
2023-10-31 11:26:42 +0100

    Show uploaded image on gallery page

---
+++ src/app/gallery/UploadButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CldUploadButton } from "next-cloudinary";
+import { useRouter } from "next/navigation";
+import { UploadResult } from "../page";
+
+const UploadButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button asChild>
+      <div className="flex gap-2 items-center">
+        <CldUploadButton
+          onUpload={(results: UploadResult) => {}}
+          uploadPreset="v0u9n21m"
+        />
+      </div>
+    </Button>
+  );
+};
+
+export default UploadButton;
